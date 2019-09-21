0b11bcb2bb1a3edfb2a0827babe487e4e4cb2f71
daniel-dorroh <daniel.dorroh@gmail.com>
2019-09-21 13:44:33 +0200

    Implemented regularized cost function.

--- ex2/output.m
+++ ex2/output.m
@@ -1,4 +1,4 @@
-function out = output(partId = '4')
+function out = output(partId = '6')
   % Random Test Cases
   X = [ones(20,1) (exp(1) * sin(1:1:20))' (exp(0.5) * cos(1:1:20))'];
   y = sin(X(:,1) + X(:,2)) > 0;
