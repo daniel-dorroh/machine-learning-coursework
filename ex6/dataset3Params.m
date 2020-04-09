fdca34fe74cc0734bc7e89fd74c48cbdde9ae476
daniel-dorroh <ddorroh.ni@gmail.com>
2020-04-08 19:57:12 -0500

    Finished optimization of sigma and C for gaussian kernel model training

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -10,6 +10,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
+error = 100000000;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,12 +23,19 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
-
-
-
-
+numbers = [0.01 0.03 0.1 0.3 1 3 10 30];
+for c = numbers
+  for sig = numbers
+    model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, sig));
+    predictions = svmPredict(model, Xval);
+    err = mean(double(predictions ~= yval))
+    if (err < error)
+      error = err;
+      C = c;
+      sigma = sig;
+    endif
+  end
+end
 
 % =========================================================================
 
