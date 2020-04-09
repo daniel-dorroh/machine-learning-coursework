ed1a4f0e7cdc6d9e6af6caa88388e1df41ffb56c
daniel-dorroh <ddorroh.ni@gmail.com>
2020-04-08 21:43:56 -0500

    Finished email feature conversion

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -96,6 +96,12 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
+    for i = 1:length(vocabList)
+        vocab_word = vocabList{i};
+        if (strcmp(str, vocab_word) == 1) 
+            word_indices(end + 1) = i;
+        endif
+    end
 
 
 
